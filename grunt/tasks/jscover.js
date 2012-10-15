197f5736c4d9ec3a6c03876521f775601b5d770e
Alex-Seville <alexander.southern@gmail.com>
2012-10-15 12:19:29 -0400

    More grunt code.  Not quite working yet.

--- grunt/tasks/jscover.js
+++ grunt/tasks/jscover.js
@@ -17,23 +17,37 @@ module.exports = function(grunt) {
   // TASKS
   // ==========================================================================
 
-  grunt.registerTask('jscover', 'code coverage', function() {
-    var testFilepath = "example/src/";
-    var testFilename = "test.js";
-    var testFile = grunt.file.read(testFilepath+testFilename);
-    var instrumentedFile = grunt.helper('jscover-instrument',testFile,testFilepath,testFilename);
+  grunt.registerMultiTask('jscover', 'code coverage', function() {
+   
+
+    var root = this.file.src;
+    var filepaths = grunt.file.expandFiles(root);
+    var testfile, instrumentedFile,file;
+    var done = this.async();
+    for(var i=0;i<filepaths.length;i++){
+      file = filepaths[i];
+      testFile = grunt.file.read(file);
+      instrumentedFile = grunt.helper('jscover-instrument',testFile,file,root,done);
+    }
   });
 
   // ==========================================================================
   // HELPERS
   // ==========================================================================
 
-  grunt.registerHelper('jscover-instrument', function(infile,infilepath,infilename) {
+  grunt.registerHelper('jscover-instrument', function(infile,infilename,rootPath,callback) {
     jsCover.instrument({
         inputFile: infile,
-       inputFileName: infilepath+infilename
+       inputFileName: infilename
     },function(result){
-        grunt.file.write("example/src-cov/"+infilename, result);
+        var newRootpath = rootPath;
+        if (newRootpath.slice(newRootpath.length) == "/"){ //replace with regex
+          newRootpath.length = newRootpath.length-1;
+        }
+        newRootpath += "-cov/";
+        infilename.replace(rootPath,newRootpath);
+        grunt.file.write(infilename, result);
+        callback();
     });
   });
 
