baeda5c7c67ec6fc0a0a5e5e2a3b46e727bcd90f
alex-seville <alexander.southern@gmail.com>
2012-11-05 08:08:03 -0500

    Fixed qunit example and changed mocha example

--- example/mocha/test/test_node_module.js
+++ example/mocha/test/test_node_module.js
@@ -1,6 +1,6 @@
-var blanket = require("blanket")("sample");
+var blanket = require("../../dist/mocha/blanket")("sample");
 var assert = require("assert");
-var sampleTest = require("./sample");
+var sampleTest = require("./nested/nested_require");
 
 describe('require test', function(){
     it('should be true', function(){
