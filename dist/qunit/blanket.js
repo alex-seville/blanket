dbc7124efdc36c4d792f77b2d0d458afb319e31f
alex-seville <alexander.southern@gmail.com>
2012-11-30 19:31:09 -0500

    Resolved merge and rebuilt

--- dist/qunit/blanket.js
+++ dist/qunit/blanket.js
@@ -3956,7 +3956,7 @@ var parseAndModify = (typeof exports === 'undefined' ? window.falafel : require(
         },
         _trackingArraySetup: [],
         _prepareSource: function(source){
-            return source.replace(/'/g,"\\'").replace(/(\r\n|\n|\r)/gm,"\n").split('\n');
+            return source.replace(/\\/g,"\\\\").replace(/'/g,"\\'").replace(/(\r\n|\n|\r)/gm,"\n").split('\n');
         },
         _trackingSetup: function(filename,sourceArray){
             
@@ -4156,7 +4156,16 @@ var Reporter = function(blanket){
         body = document.body,
         headerContent,
         bodyContent = "<div id='blanket-main'><div class='blanket bl-title'><div class='bl-cl bl-file'><a href='http://migrii.github.com/blanket/' target='_blank' class='bl-logo'>Blanket.js</a> results</div><div class='bl-cl rs'>Coverage (%)</div><div class='bl-cl rs'>Covered/Total Smts.</div><div style='clear:both;'></div></div>",
-        fileTemplate = "<div class='blanket {{statusclass}}'><div class='bl-cl bl-file'><span class='bl-nb'>{{fileNumber}}.</span><a href='javascript:toggleSource(\"file-{{fileNumber}}\")'>{{file}}</a></div><div class='bl-cl rs'>{{percentage}} %</div><div class='bl-cl rs'>{{numberCovered}}/{{totalSmts}}</div><div id='file-{{fileNumber}}' class='bl-source' style='display:none;'>{{source}}</div><div style='clear:both;'></div></div>";
+        fileTemplate = "<div class='blanket {{statusclass}}'><div class='bl-cl bl-file'><span class='bl-nb'>{{fileNumber}}.</span><a href='javascript:blanket.toggleSource(\"file-{{fileNumber}}\")'>{{file}}</a></div><div class='bl-cl rs'>{{percentage}} %</div><div class='bl-cl rs'>{{numberCovered}}/{{totalSmts}}</div><div id='file-{{fileNumber}}' class='bl-source' style='display:none;'>{{source}}</div><div style='clear:both;'></div></div>";
+
+    window.blanket.toggleSource = function(id) {
+        var element = document.getElementById(id);
+        if(element.style.display === 'block') {
+            element.style.display = 'none';
+        } else {
+            element.style.display = 'block';
+        }
+    };
 
     var percentage = function(number, total) {
         return (Math.round(((number/total) * 100)*100)/100);
@@ -4167,6 +4176,14 @@ var Reporter = function(blanket){
         dom.innerHTML = str;
         el.appendChild(dom);
     };
+
+    function escapeInvalidXmlChars(str) {
+        return str.replace(/\&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/\>/g, "&gt;")
+            .replace(/\"/g, "&quot;")
+            .replace(/\'/g, "&apos;");
+    }
 
     var files = blanket.files;
     for(var file in files)
@@ -4179,15 +4196,15 @@ var Reporter = function(blanket){
             code = [],
             i;
 
-        for(i = 0; i < statsForFile.source.length; i ++){
-            code[i] = "<div class='{{executed}}'><span class=''>"+(i+1)+"</span>"+statsForFile.source[i]+"</div>";
+        for(i = 0; i < statsForFile.source.length; i +=1){
+            code[i + 1] = "<div class='{{executed}}'><span class=''>"+(i + 1)+"</span>"+escapeInvalidXmlChars(statsForFile.source[i])+"</div>";
         }
 
-        for(i = 0; i < statsForFile.length; i++)
+        for(i = 1; i < statsForFile.length; i++)
         {
-            if(statsForFile[i+1]) {
-                totalSmts++;
-                numberOfFilesCovered++;
+            if(statsForFile[i]) {
+                numberOfFilesCovered += 1;
+                totalSmts += 1;
                 code[i] = code[i].replace("{{executed}}",'hit');
             }else{
                 if(statsForFile[i+1] === 0){
@@ -4218,8 +4235,6 @@ var Reporter = function(blanket){
     //appendStyle(body, headerContent);
     appendTag('div', body, bodyContent);
     //appendHtml(body, '</div>');
-    appendTag('script', head, script);
-
 };
 
 
