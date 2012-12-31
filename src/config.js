21eee686d8c1b57eebb844f79e28a361c0ba18ef
alex-seville <alexander.southern@gmail.com>
2012-12-31 15:15:52 -0500

    Need dist and src too

---
+++ src/config.js
@@ -0,0 +1,34 @@
+(function(){
+    var newOptions={};
+    //http://stackoverflow.com/a/2954896
+    var toArray =Array.prototype.slice;
+    var scripts = toArray.call(document.scripts);
+    toArray.call(scripts[scripts.length - 1].attributes)
+                    .forEach(function(es){
+                        if(es.nodeName === "data-cover-only"){
+                            newOptions.filter = es.nodeValue;
+                        }
+                        if(es.nodeName === "data-cover-reporter"){
+                            newOptions.reporter = es.nodeValue;
+                        }
+                        if (es.nodeName === "data-cover-adapter"){
+                            newOptions.adapter = es.nodeValue;
+                        }
+                        if (es.nodeName === "data-cover-loader"){
+                            newOptions.loader = es.nodeValue;
+                        }
+                        if (es.nodeName === "data-cover-flags"){
+                            var flags = " "+es.nodeValue+" ";
+                            if (flags.indexOf(" unordered ") > -1){
+                                newOptions.order = false;
+                            }
+                            if (flags.indexOf(" ignoreError ") > -1){
+                                newOptions.ignoreScriptError = true;
+                            }
+                            if (flags.indexOf(" autoStart ") > -1){
+                                newOptions.autoStart = true;
+                            }
+                        }
+                    });
+    blanket.options(newOptions);
+})();
