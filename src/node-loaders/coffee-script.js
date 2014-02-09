01ad1dd69fc54d413e22fada2e3456d6ac9bea0c
Ricky Chien <ricky060709@gmail.com>
2014-02-09 20:15:17 +0800

    Improve coding style

--- src/node-loaders/coffee-script.js
+++ src/node-loaders/coffee-script.js
@@ -1,33 +1,32 @@
 var fs = require("fs"),
     path = require("path");
 
-module.exports = function(blanket){
-    var coffeeScript = require("coffee-script");
-    var oldLoaderCS = require.extensions['.coffee'];
+module.exports = function(blanket) {
+    var coffeeScript = require("coffee-script"),
+        oldLoaderCS = require.extensions['.coffee'];
 
     require.extensions['.coffee'] = function(localModule, filename) {
-        
+
         var pattern = blanket.options("filter");
         filename = blanket.normalizeBackslashes(filename);
-        if (blanket.matchPattern(filename,pattern)){
 
+        if (blanket.matchPattern(filename, pattern)) {
             var content = fs.readFileSync(filename, 'utf8');
             content = coffeeScript.compile(content);
             blanket.instrument({
                 inputFile: content,
                 inputFileName: filename
-            },function(instrumented){
-                var baseDirPath = blanket.normalizeBackslashes(path.dirname(filename))+'/.';
-                try{
-                    instrumented = instrumented.replace(/require\s*\(\s*("|')\./g,'require($1'+baseDirPath);
+            }, function(instrumented) {
+                var baseDirPath = blanket.normalizeBackslashes(path.dirname(filename)) + '/.';
+                try {
+                    instrumented = instrumented.replace(/require\s*\(\s*("|')\./g, 'require($1' + baseDirPath);
                     localModule._compile(instrumented, filename);
-                }
-                catch(err){
-                    console.log("Error parsing instrumented code: "+err);
+                } catch (err) {
+                    console.log("Error parsing instrumented code: " + err);
                 }
             });
-        }else{
-            oldLoaderCS(localModule,filename);
+        } else {
+            oldLoaderCS(localModule, filename);
         }
     };
 };
