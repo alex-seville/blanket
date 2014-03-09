c81a61296b9b78c255933683baf2e94e3a11df91
Joe Dallago <jd.dallago@gmail.com>
2014-03-09 17:34:55 +0000

    Ran grunt, committing the distribution files.

--- dist/mocha/blanket_mocha.js
+++ dist/mocha/blanket_mocha.js
@@ -5417,6 +5417,8 @@ _blanket.extend({
             OriginalReporter.apply(this, arguments);
         };
 
+    BlanketReporter.prototype = mocha._reporter.prototype;
+
     mocha.reporter(BlanketReporter);
 
     var oldRun = mocha.run,
