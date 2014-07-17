05958d2be41029b937ef4dfe2cd0f483e49b5c1a
Alex Seville <alexander.southern@gmail.com>
2014-07-17 11:56:46 -0700

    Revert "Remove invalid uppercase characters in data-cover attributes"

--- dist/jasmine/blanket_jasmine.js
+++ dist/jasmine/blanket_jasmine.js
@@ -5114,11 +5114,11 @@ blanket.defaultReporter = function(coverage) {
                 }
             }
 
-            if (es.nodeName === "data-cover-testreadycallback") {
+            if (es.nodeName === "data-cover-testReadyCallback") {
                 newOptions.testReadyCallback = es.nodeValue;
             }
 
-            if (es.nodeName === "data-cover-customvariable") {
+            if (es.nodeName === "data-cover-customVariable") {
                 newOptions.customVariable = es.nodeValue;
             }
 
